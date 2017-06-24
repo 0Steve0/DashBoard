b73a693eef671aa405f489d812c3851233e1242c
Steve <l7shao@uwaterlo.ca>
2017-06-24 17:48:53 -0400

    add database connection and front end

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 //import  packages
 const express = require('express');
+const mongoose = require('mongoose');
 const user = require('./routers/users');
+/*
+ * Connect to the database
+ * Connected and error show differnet messages
+ */
+const config = require('./config/database');
+mongoose.connect(config.database);
+mongoose.connection.on('connected',function(){
+  console.log('Connected to database: '+config.database);
+});
+mongoose.connection.on('error',function(err){
+  console.log('Database error'+err);
+});
 //initialize app and set port
 const app = express();
 const port = process.env.PORT||8080;
 
-
-
-app.use('/user',user);
+app.use('/',user);
 
 app.listen(port,function(){
   console.log('Server is running at: ' + port)
---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //use admin accout in this demo version of website
+  database:'mongodb://admin:admin@ds135532.mlab.com:35532/steveshao_db',
+  secret:'secret'
+}
